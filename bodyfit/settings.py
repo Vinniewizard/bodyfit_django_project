0ac6d25e853f5346ba30e6ff163cd8009850be25
Vinniewizard <vincentkipkoech818@gmail.com>
2026-05-20 17:29:19 +0300

    Fix the errors

--- bodyfit/settings.py
+++ bodyfit/settings.py
@@ -89,11 +89,13 @@ DATABASES = {
 
 DATABASE_URL = config('DATABASE_URL', default='')
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(
-        DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=not DEBUG,
-    )
+    database_config = {
+        'conn_max_age': 600,
+    }
+    if DATABASE_URL.startswith(('postgres://', 'postgresql://')):
+        database_config['ssl_require'] = not DEBUG
+
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, **database_config)
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
